9c3721a6753a54ecf7760483fb9b4d0d11bd5d93
vrushank1809 <112951979+vrushank1809@users.noreply.github.com>
2022-09-06 18:24:46 +0530

    Add files via upload

---
+++ space flutter/screens/detail.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:space_flutter/constant.dart';
+import 'package:space_flutter/data.dart';
+
+class DetailPage extends StatelessWidget {
+  final PlanetInfo planetInfo;
+
+  const DetailPage({Key key, this.planetInfo}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(height: 300),
+                        Text(
+                          planetInfo.name,
+                          style: TextStyle(
+                            fontFamily: 'Avenier',
+                            fontSize: 56,
+                            color: primaryTextColor,
+                            fontWeight: FontWeight.w900,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        Text(
+                          'Solar System',
+                          style: TextStyle(
+                            fontFamily: 'Avenier',
+                            fontSize: 31,
+                            color: primaryTextColor,
+                            fontWeight: FontWeight.w300,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        Divider(color: Colors.black38,),
+                        SizedBox(height: 32),
+                        Text(
+                          planetInfo.description ?? '',
+                          maxLines: 5,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontFamily: 'Avenier',
+                            fontSize: 20,
+                            color: contentTextColor,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 32),
+                        Divider(color: Colors.black38),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 32.0),
+                    child: Text(
+                      'Gallary',
+                      style: TextStyle(
+                        fontFamily: 'Avenier',
+                        fontSize: 25,
+                        color: const Color(0xff47455f),
+                        fontWeight: FontWeight.w300,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Container(
+                    height: 250,
+                    padding: const EdgeInsets.only(left: 32.0),
+                    child: ListView.builder(
+                      itemCount: planetInfo.images.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.network(
+                              planetInfo.images[index],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      }
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              top: 60,
+              left: 32,
+              child: Text(
+                planetInfo.position.toString(),
+                style: TextStyle(
+                  fontFamily: 'Avenier',
+                  fontSize: 247,
+                  color: primaryTextColor.withOpacity(0.08),
+                  fontWeight: FontWeight.w900,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
